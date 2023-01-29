feb12c614e0c9cc88ce206238fdc94d8ff77d031
Sergei Vasiliev <9670105747@mail.ru>
2023-01-29 20:16:18 +0700

    Задача 8 четные числа в интервале

---
+++ C#_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), на выходе показывает все четные числа от еденицы до N.
+// 5 -> 2, 4
+// 8 -> 2,4,6,8
+
+int numberM = 1;
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+for (int i = numberM; i <= numberN; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + " ");
+    }
+}
